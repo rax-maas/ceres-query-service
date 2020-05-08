ee92ff58f33ddcde236f23e20cf6194c1d0fbd43
Geoff Bourne <geoff.bourne@rackspace.com>
2020-05-08 08:46:47 -0500

    Fixed dev access to grafana URL with curl (#24)
    Also
    * defaulted to production profile in yaml
    * defaulted to development profile in spring-boot:run

--- src/test/java/com/rackspacecloud/metrics/queryservice/QueryServiceImplTests.java
+++ src/test/java/com/rackspacecloud/metrics/queryservice/QueryServiceImplTests.java
@@ -31,11 +31,13 @@ import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class QueryServiceImplTests {
     @MockBean
     private RouteProvider routeProvider;
